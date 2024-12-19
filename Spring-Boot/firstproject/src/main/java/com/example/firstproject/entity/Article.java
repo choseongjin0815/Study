a129c1ecd4f841245d2f2d2c7b9c74d715ede4dc
choseongin0815 <chobocho0815@gmail.com>
2024-12-19 15:26:48 +0900

    springboot

--- Spring-Boot/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ Spring-Boot/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -15,7 +12,7 @@ import lombok.ToString;
 public class Article {
     //@Id어노테이션은 기본키 역할하게 해줌
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
--- Spring-Boot/firstproject/src/main/resources/data.sql
+++ Spring-Boot/firstproject/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나나', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
 
-INSERT INTO member(id, email, pwd) VALUES(1, 'ijn10000@naver.com', '1111');
-INSERT INTO member(id, email, pwd) VALUES(2, 'okm10000@naver.com', '2222');
-INSERT INTO member(id, email, pwd) VALUES(3, 'chobocho0815@gmail.com', '3333');
+INSERT INTO member(email, pwd) VALUES('ijn10000@naver.com', '1111');
+INSERT INTO member(email, pwd) VALUES('okm10000@naver.com', '2222');
+INSERT INTO member(email, pwd) VALUES('chobocho0815@gmail.com', '3333');
