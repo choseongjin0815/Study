f589b3d2fa822386ddf41fc2910f53d21a386739
choseongin0815 <chobocho0815@gmail.com>
2026-01-08 20:50:45 +0900

    2026-01-08, 프록시, 연관관계 관리

--- jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -77,6 +77,20 @@ public class JpaMain {
 //
 //            Movie findMovie = em.find(Movie.class, movie.getId());
 
+            Child child = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+            parent.addChild(child);
+            parent.addChild(child2);
+
+            em.persist(parent);
+
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
 
             tx.commit();
