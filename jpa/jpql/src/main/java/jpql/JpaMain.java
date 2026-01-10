813ec816a879630d1653f40f892f767340cfd3b5
choseongin0815 <chobocho0815@gmail.com>
2026-01-10 22:24:07 +0900

    2026-01-10, JPQL

--- jpa/jpql/src/main/java/jpql/JpaMain.java
+++ jpa/jpql/src/main/java/jpql/JpaMain.java
@@ -16,19 +16,75 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
+            Team team = new Team();
+            team.setName("team");
+            em.persist(team);
+
+            Team team2 = new Team();
+            team2.setName("team2");
+            em.persist(team2);
+
+
             Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(20);
+            member.setUsername("member");
+            member.setTeam(team);
+            member.setAge(0);
             em.persist(member);
 
-            em.flush();
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team);
+            member2.setAge(0);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setTeam(team2);
+            member3.setAge(0);
+            em.persist(member3);
+
+//            String query = "select m from Member m join fetch m.team";
+
+            String query = "select m from Member m where m = :member";
+//            em.flush();
+//            em.clear();
+
+            int resultCount = em.createQuery("update Member m set m.age = 3").executeUpdate();
+
             em.clear();
 
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-            MemberDTO memberDTO = resultList.get(0);  
-            System.out.println("username: " + memberDTO.getUsername());
-            System.out.println("age: " + memberDTO.getAge());
+            //영속성 컨텍스트에는 0으로 그대로 있고 DB에만 업데이트 됨 따라서 3이 아닌 0이 나옴
+            Member findMember = em.find(Member.class, member.getId());
+            System.out.println("find member: " + findMember.getAge());
+
+            System.out.println(resultCount);
+//
+//            Member findMember = em.createQuery(query , Member.class).setParameter("member", member).getSingleResult();
+//            System.out.println("Found member: " + findMember);
+//            List<Member> result = em.createQuery(query, Member.class).getResultList();
+//
+//            for (Member m : result) {
+//                System.out.println("Member: " + m.getUsername() + "," + m.getTeam().getName());
+//
+//                //join fetch안쓰면
+//                //회원1  팀A(SQL)
+//                //회원2 팀A(1차캐시)
+//                //회원3, 팀B(SQL)
+//            }
+            //            Member member = new Member();
+//            member.setUsername("member1");
+//            member.setAge(20);
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
+//
+//            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+//                    .getResultList();
+//            MemberDTO memberDTO = resultList.get(0);
+//            System.out.println("username: " + memberDTO.getUsername());
+//            System.out.println("age: " + memberDTO.getAge());
 //           TypedQuery<Member> query =  em.createQuery("select m from Member m where m.username = :username", Member.class);
 //           query.setParameter("username", "member1");
 //           Member member1 = query.getSingleResult();
