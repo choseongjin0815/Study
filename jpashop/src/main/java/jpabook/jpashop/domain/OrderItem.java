0a363077c01914c5cd84c0df9e87dbb7500d2e3e
choseongin0815 <chobocho0815@gmail.com>
2026-01-14 19:38:39 +0900

    2026-01-14, 주문 도메인 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,14 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +27,26 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; //주문수량
 
+
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); //주문한 만큼 재고를 감소시킨다.
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count); //재고 원복
+    }
+
+    //조회 로직 (주문상품전체가격조회)
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
