baa761ef128d567a8e7eda301eb16d20e464021c
choseongin0815 <chobocho0815@gmail.com>
2026-01-15 21:28:34 +0900

    2026-01-15, xToOne 조회 최적화

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +17,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+//    @NotEmpty(message = "이름은 필수값입니다.")
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "member") //order 테이블에 있는 member 필드에 따라, 읽기 전용임
     private List<Order> orders = new ArrayList<>();
 
