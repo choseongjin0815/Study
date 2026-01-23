fdbab25fa79894ec6d4eaa80fc81494cf3bb93c1
choseongin0815 <chobocho0815@gmail.com>
2026-01-23 15:21:29 +0900

    2026-01-23, 순수 JPA와 QueryDSL

---
+++ querydsl/src/main/java/study/querydsl/controller/HelloController.java
@@ -0,0 +1,9 @@
+package study.querydsl.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+}
--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -660,5 +660,6 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+
 }
 
